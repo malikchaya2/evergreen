fada1cf2001784fae21ecd10a99d67adbb9fc28b
Chaya Malik <chaya.malik@10gen.com>
2020-11-02 16:29:28 -0500

    add user sourced info

--- model/task_annotations/task_annotations.go
+++ model/task_annotations/task_annotations.go
@@ -11,15 +11,30 @@ type TaskAnnotation struct {
 	TaskId        string `bson:"task_id" json:"task_id"`
 	TaskExecution int    `bson:"task_execution" json:"task_execution"`
 	// comment about the failure
-	Note string `bson:"note,omitempty" json:"note,omitempty"`
+	Note Note `bson:"note,omitempty" json:"note,omitempty"`
 	// links to tickets definitely related.
-	Issues []IssueLink `bson:"issues,omitempty" json:"issues,omitempty"`
+	Issues Issues `bson:"issues,omitempty" json:"issues,omitempty"`
 	// links to tickets possibly related
-	SuspectedIssues []IssueLink `bson:"suspected_issues,omitempty" json:"suspected_issues,omitempty"`
+	SuspectedIssues SuspectedIssues `bson:"suspected_issues,omitempty" json:"suspected_issues,omitempty"`
 	// annotation attribution
 	Source AnnotationSource `bson:"source" json:"source"`
 	// structured data about the task (not displayed in the UI, but available in the API)
 	Metadata *birch.Document `bson:"metadata,omitempty" json:"metadata,omitempty"`
+}
+
+type Note struct {
+	Note     string `bson:"note,omitempty" json:"note,omitempty"`
+	UserNote string `bson:"user_note,omitempty" json:"user_note,omitempty"`
+}
+
+type Issues struct {
+	Issues     []IssueLink `bson:"issues,omitempty" json:"issues,omitempty"`
+	UserIssues []IssueLink `bson:"user_issues,omitempty" json:"user_issues,omitempty"`
+}
+
+type SuspectedIssues struct {
+	SuspectedIssues     []IssueLink `bson:"suspected_issues,omitempty" json:"suspected_issues,omitempty"`
+	UserSuspectedIssues []IssueLink `bson:"user_suspected_issues,omitempty" json:"user_suspected_issues,omitempty"`
 }
 
 type IssueLink struct {
