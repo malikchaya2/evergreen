9bc1e940270135a59fd62c5d36f95caa485e8c64
Chaya Malik <chaya.malik@10gen.com>
2022-03-16 11:35:16 -0400

    add repo_event

---
+++ rest/model/repo_event.go
@@ -0,0 +1,230 @@
+package model
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/evergreen-ci/evergreen/model"
+	"github.com/evergreen-ci/evergreen/model/event"
+	"github.com/evergreen-ci/utility"
+	"github.com/mongodb/grip"
+	"github.com/pkg/errors"
+)
+
+type APIRepoEvent struct {
+	Timestamp *time.Time      `json:"ts"`
+	User      *string         `json:"user"`
+	Before    APIRepoSettings `json:"before"`
+	After     APIRepoSettings `json:"after"`
+}
+
+type APIRepoSettings struct {
+	RepoRef               APIProjectRef     `json:"proj_ref"`
+	GitHubWebhooksEnabled bool              `json:"github_webhooks_enabled"`
+	Vars                  APIRepoVars       `json:"vars"`
+	Aliases               []APIRepoAlias    `json:"aliases"`
+	Subscriptions         []APISubscription `json:"subscriptions"`
+}
+
+type APIRepoVars struct {
+	Vars          map[string]string `json:"vars"`
+	PrivateVars   map[string]bool   `json:"private_vars"`
+	AdminOnlyVars map[string]bool   `json:"admin_only_vars"`
+	VarsToDelete  []string          `json:"vars_to_delete,omitempty"`
+
+	// to use for the UI
+	PrivateVarsList   []string `json:"-"`
+	AdminOnlyVarsList []string `json:"-"`
+}
+
+type APIRepoAlias struct {
+	Alias       *string   `json:"alias"`
+	GitTag      *string   `json:"git_tag"`
+	Variant     *string   `json:"variant"`
+	Task        *string   `json:"task"`
+	RemotePath  *string   `json:"remote_path"`
+	VariantTags []*string `json:"variant_tags,omitempty"`
+	TaskTags    []*string `json:"tags,omitempty"`
+	Delete      bool      `json:"delete,omitempty"`
+	ID          *string   `json:"_id,omitempty"`
+}
+
+func (e *APIRepoEvent) BuildFromService(h interface{}) error {
+	switch v := h.(type) {
+	case model.ProjectChangeEventEntry:
+		e.Timestamp = ToTimePtr(v.Timestamp)
+		data, ok := v.Data.(*model.ProjectChangeEvent)
+		if !ok {
+			return errors.New("unable to convert event data to project change")
+		}
+
+		user := utility.ToStringPtr(data.User)
+		before, err := DbRepoSettingsToRestModel(data.Before)
+		if err != nil {
+			return errors.Wrap(err, "unable to convert 'before' changes")
+		}
+		after, err := DbRepoSettingsToRestModel(data.After)
+		if err != nil {
+			return errors.Wrap(err, "unable to convert 'after' changes")
+		}
+
+		e.User = user
+		e.Before = before
+		e.After = after
+	default:
+		return fmt.Errorf("%T is not the correct event type", h)
+	}
+
+	return nil
+}
+
+func (e *APIRepoEvent) ToService() (interface{}, error) {
+	return nil, errors.New("ToService not implemented for APIRepoEvent")
+}
+
+func DbRepoSettingsToRestModel(settings model.ProjectSettings) (APIRepoSettings, error) {
+	apiRepoRef := APIProjectRef{}
+	if err := apiRepoRef.BuildFromService(settings.ProjectRef); err != nil {
+		return APIRepoSettings{}, err
+	}
+
+	apiSubscriptions, err := DbRepoSubscriptionsToRestModel(settings.Subscriptions)
+	if err != nil {
+		return APIRepoSettings{}, err
+	}
+
+	apiRepoVars := APIRepoVars{}
+	if err := apiRepoVars.BuildFromService(&settings.Vars); err != nil {
+		return APIRepoSettings{}, err
+	}
+
+	return APIRepoSettings{
+		RepoRef:               apiRepoRef,
+		GitHubWebhooksEnabled: settings.GitHubHooksEnabled,
+		Vars:                  apiRepoVars,
+		Aliases:               DbRepoAliasesToRestModel(settings.Aliases),
+		Subscriptions:         apiSubscriptions,
+	}, nil
+}
+
+func (p *APIRepoVars) ToService() (interface{}, error) {
+	privateVars := map[string]bool{}
+	adminOnlyVars := map[string]bool{}
+	// ignore false inputs
+	for key, val := range p.PrivateVars {
+		if val {
+			privateVars[key] = val
+		}
+	}
+	for key, val := range p.AdminOnlyVars {
+		if val {
+			adminOnlyVars[key] = val
+		}
+	}
+
+	// handle UI list
+	for _, each := range p.PrivateVarsList {
+		privateVars[each] = true
+	}
+	for _, each := range p.AdminOnlyVarsList {
+		adminOnlyVars[each] = true
+	}
+	return &model.ProjectVars{
+		Vars:          p.Vars,
+		AdminOnlyVars: adminOnlyVars,
+		PrivateVars:   privateVars,
+	}, nil
+}
+
+func (p *APIRepoVars) BuildFromService(h interface{}) error {
+	switch v := h.(type) {
+	case *model.ProjectVars:
+		p.PrivateVars = v.PrivateVars
+		p.Vars = v.Vars
+		p.AdminOnlyVars = v.AdminOnlyVars
+	default:
+		return errors.New("Invalid type of the argument")
+	}
+	return nil
+}
+
+func (a *APIRepoAlias) ToService() (interface{}, error) {
+	res := model.ProjectAlias{
+		Alias:       utility.FromStringPtr(a.Alias),
+		Task:        utility.FromStringPtr(a.Task),
+		Variant:     utility.FromStringPtr(a.Variant),
+		GitTag:      utility.FromStringPtr(a.GitTag),
+		RemotePath:  utility.FromStringPtr(a.RemotePath),
+		TaskTags:    utility.FromStringPtrSlice(a.TaskTags),
+		VariantTags: utility.FromStringPtrSlice(a.VariantTags),
+	}
+	if model.IsValidId(utility.FromStringPtr(a.ID)) {
+		res.ID = model.NewId(utility.FromStringPtr(a.ID))
+	}
+	return res, nil
+}
+
+func (a *APIRepoAlias) BuildFromService(h interface{}) error {
+	switch v := h.(type) {
+	case *model.ProjectAlias:
+		APITaskTags := utility.ToStringPtrSlice(v.TaskTags)
+		APIVariantTags := utility.ToStringPtrSlice(v.VariantTags)
+
+		a.Alias = utility.ToStringPtr(v.Alias)
+		a.Variant = utility.ToStringPtr(v.Variant)
+		a.GitTag = utility.ToStringPtr(v.GitTag)
+		a.RemotePath = utility.ToStringPtr(v.RemotePath)
+		a.Task = utility.ToStringPtr(v.Task)
+		a.VariantTags = APIVariantTags
+		a.TaskTags = APITaskTags
+		a.ID = utility.ToStringPtr(v.ID.Hex())
+	case model.ProjectAlias:
+		APITaskTags := utility.ToStringPtrSlice(v.TaskTags)
+		APIVariantTags := utility.ToStringPtrSlice(v.VariantTags)
+
+		a.Alias = utility.ToStringPtr(v.Alias)
+		a.Variant = utility.ToStringPtr(v.Variant)
+		a.GitTag = utility.ToStringPtr(v.GitTag)
+		a.RemotePath = utility.ToStringPtr(v.RemotePath)
+		a.Task = utility.ToStringPtr(v.Task)
+		a.VariantTags = APIVariantTags
+		a.TaskTags = APITaskTags
+		a.ID = utility.ToStringPtr(v.ID.Hex())
+	default:
+		return errors.New("Invalid type of argument")
+	}
+	return nil
+}
+
+func DbRepoAliasesToRestModel(aliases []model.ProjectAlias) []APIRepoAlias {
+	result := []APIRepoAlias{}
+	for _, alias := range aliases {
+		apiAlias := APIRepoAlias{
+			Alias:       utility.ToStringPtr(alias.Alias),
+			Variant:     utility.ToStringPtr(alias.Variant),
+			Task:        utility.ToStringPtr(alias.Task),
+			RemotePath:  utility.ToStringPtr(alias.RemotePath),
+			GitTag:      utility.ToStringPtr(alias.GitTag),
+			TaskTags:    utility.ToStringPtrSlice(alias.TaskTags),
+			VariantTags: utility.ToStringPtrSlice(alias.VariantTags),
+		}
+		result = append(result, apiAlias)
+	}
+
+	return result
+}
+
+func DbRepoSubscriptionsToRestModel(subscriptions []event.Subscription) ([]APISubscription, error) {
+	catcher := grip.NewBasicCatcher()
+	apiSubscriptions := []APISubscription{}
+	for _, subscription := range subscriptions {
+		apiSubscription := APISubscription{}
+		if err := apiSubscription.BuildFromService(subscription); err != nil {
+			catcher.Add(err)
+			continue
+		}
+		apiSubscriptions = append(apiSubscriptions, apiSubscription)
+	}
+
+	return apiSubscriptions, catcher.Resolve()
+}
