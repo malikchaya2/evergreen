fed14de2f7f092ed6696d4cee0cdda23b32b2c6f
chaya <malikchaya2@gmail.com>
2025-09-10 10:43:32 -0400

    with debug

--- rest/route/host_agent.go
+++ rest/route/host_agent.go
@@ -1342,9 +1342,14 @@ func (h *hostAgentEndTask) Run(ctx context.Context) gimlet.Responder {
 			Description: evergreen.TaskDescriptionAborted,
 		}
 	}
+	grip.Debug(message.Fields{
+		"message":                 "chayaMtesting in hostAgentEndTask",
+		"details.Status":          details.Status,
+		"t.ShouldUseFailedBucket": t.UsesLongRetentionBucket(h.env.Settings()),
+	})
 
 	// Move logs to failed bucket after marking task complete if the task failed and it should be moved
-	if details.Status == evergreen.TaskFailed && t.ShouldUseFailedBucket(h.env.Settings()) {
+	if details.Status == evergreen.TaskFailed && !t.UsesLongRetentionBucket(h.env.Settings()) {
 		if err := t.MoveTestAndTaskLogsToFailedBucket(ctx, h.env.Settings()); err != nil {
 			grip.Error(errors.Wrap(err, "moving logs to failed bucket"))
 		}
