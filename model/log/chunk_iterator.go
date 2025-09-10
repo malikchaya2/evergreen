710fdbffaf5b5014ff7d44224342265c50fed0c6
chaya <malikchaya2@gmail.com>
2025-09-10 16:59:30 -0400

    cleanup

--- model/log/chunk_iterator.go
+++ model/log/chunk_iterator.go
@@ -27,7 +27,7 @@ type chunkIterator struct {
 
 type chunkIteratorOptions struct {
 	bucket    pail.Bucket
-	chunks    []chunkInfo
+	chunks    []ChunkInfo
 	parser    LineParser
 	start     *int64
 	end       *int64
@@ -138,14 +138,14 @@ func (it *chunkIterator) worker(ctx context.Context) {
 	}()
 
 	for _, chunk := range it.opts.chunks {
-		r, err := it.opts.bucket.Get(ctx, chunk.key)
+		r, err := it.opts.bucket.Get(ctx, chunk.Key)
 		if err != nil {
 			it.catcher.Wrap(err, "getting chunk from bucket")
 			return
 		}
 
 		select {
-		case it.next <- newChunkReader(r, chunk.numLines):
+		case it.next <- newChunkReader(r, chunk.NumLines):
 		case <-ctx.Done():
 			it.catcher.Add(ctx.Err())
 			return
@@ -171,10 +171,10 @@ func (it *chunkIterator) Close() error {
 	return nil
 }
 
-func filterChunksByTimeRange(chunks []chunkInfo, start, end *int64) []chunkInfo {
-	var filteredChunks []chunkInfo
+func filterChunksByTimeRange(chunks []ChunkInfo, start, end *int64) []ChunkInfo {
+	var filteredChunks []ChunkInfo
 	for i := 0; i < len(chunks); i++ {
-		if (end != nil && utility.FromInt64Ptr(end) < chunks[i].start) || utility.FromInt64Ptr(start) > chunks[i].end {
+		if (end != nil && utility.FromInt64Ptr(end) < chunks[i].Start) || utility.FromInt64Ptr(start) > chunks[i].End {
 			continue
 		}
 		filteredChunks = append(filteredChunks, chunks[i])
@@ -183,20 +183,20 @@ func filterChunksByTimeRange(chunks []chunkInfo, start, end *int64) []chunkInfo 
 	return filteredChunks
 }
 
-func filterChunksByTailN(chunks []chunkInfo, tailN int) ([]chunkInfo, int) {
+func filterChunksByTailN(chunks []ChunkInfo, tailN int) ([]ChunkInfo, int) {
 	var numChunks, lineCount int
 	for i := len(chunks) - 1; i >= 0 && lineCount < tailN; i-- {
-		lineCount += chunks[i].numLines
+		lineCount += chunks[i].NumLines
 		numChunks++
 	}
 
 	return chunks[len(chunks)-numChunks:], lineCount - tailN
 }
 
-func filterChunksByLimit(chunks []chunkInfo, limit int) []chunkInfo {
+func filterChunksByLimit(chunks []ChunkInfo, limit int) []ChunkInfo {
 	var numChunks, lineCount int
 	for i := 0; i < len(chunks) && lineCount < limit; i++ {
-		lineCount += chunks[i].numLines
+		lineCount += chunks[i].NumLines
 		numChunks++
 	}
 
